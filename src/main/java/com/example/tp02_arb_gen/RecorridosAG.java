f37c1d53dd2b3ab218fb79173f48c058fb552d37
caracio35 <caracio35@gmail.com>
2025-08-30 13:49:08 -0300

    comentarios

--- src/main/java/com/example/tp02_arb_gen/RecorridosAG.java
+++ src/main/java/com/example/tp02_arb_gen/RecorridosAG.java
@@ -6,19 +6,36 @@ public class RecorridosAG {
 
     public ListaGenerica<Integer> numerosImparesMayoresQuePreOrden(ArbolGeneral<Integer> a, Integer n) {
         return null;
+        /*
+         * Método que retorna una lista con los elementos impares del árbol “a” que sean
+         * mayores al valor “n” pasados como parámetros, recorrido en preorden.
+         */
 
     }
 
     public ListaGenerica<Integer> numerosImparesMayoresQueInOrden(ArbolGeneral<Integer> a, Integer n) {
         return null;
+        /*
+         * Método que retorna una lista con los elementos impares del árbol “a” que sean
+         * mayores al valor “n” pasados como parámetros, recorrido en inorden.
+         */
     }
 
     public ListaGenerica<Integer> numerosImparesMayoresQuePostOrden(ArbolGeneral<Integer> a, Integer n) {
         return null;
+        /*
+         * Método que retorna una lista con los elementos impares del árbol “a” que sean
+         * mayores al valor “n” pasados como parámetros recorrido en postorden.
+         */
+
     }
 
     public ListaGenerica<Integer> numerosImparesMayoresQuePorNiveles(ArbolGeneral<Integer> a, Integer n) {
         return null;
+        /*
+         * Método que retorna una lista con los elementos impares del árbol “a” que sean
+         * mayores al valor “n” pasados como parámetros, recorrido por niveles.
+         */
     }
 
 }
